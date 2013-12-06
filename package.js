cb2ec807404f1aa8e309a1595b0502bdd14abf2d
James Stone <jamesmanofstone@gmail.com>
2013-12-06 15:25:47 +0100

    working now from /packages vs /client/packages

--- package.js
+++ package.js
@@ -1,38 +1,38 @@
-// Package.describe({
-//   summary: "A meteor package for foundation 5 to use with scss - the most advanced front-end framework in the world."
-// });
+Package.describe({
+  summary: "A meteor package for foundation 5 to use with scss - the most advanced front-end framework in the world."
+});
 
-// Package.on_use(function (api) {
-//   api.use('jquery', 'client');
+Package.on_use(function (api) {
+  api.use('jquery', 'client');
 
-//   api.add_files(path.join(asset_path, 'js/vendor/custom.modernizr.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/vendor/custom.modernizr.js'), 'client');
 
 
-//   api.add_files(path.join(asset_path, 'js/_first/foundation.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/_first/foundation.js'), 'client');
 
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.abide.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.accordian.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.alert.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.clearing.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.dropdown.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.interchange.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.joyride.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.magellan.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.offcanvas.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.orbit.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.reveal.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.tab.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.tolltip.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/foundation/foundation.topbar.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.abide.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.accordian.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.alert.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.clearing.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.dropdown.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.interchange.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.joyride.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.magellan.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.offcanvas.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.orbit.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.reveal.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.tab.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.tolltip.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/foundation/foundation.topbar.js'), 'client');
 
-//   // var path = Npm.require('path'),
-//   //     asset_path = path.join('foundation');
+  // var path = Npm.require('path'),
+  //     asset_path = path.join('foundation');
 
-//   // js
-//   api.add_files(path.join(asset_path, 'js/vendor/fastclick.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/vendor/jquery.autocomplete.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/vendor/jquery.cookie.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/vendor/placeholder.js'), 'client');
-//   api.add_files(path.join(asset_path, 'js/zzz_last/activate-foundation.js'), 'client');
+  // js
+  api.add_files(path.join(asset_path, 'js/vendor/fastclick.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/vendor/jquery.autocomplete.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/vendor/jquery.cookie.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/vendor/placeholder.js'), 'client');
+  api.add_files(path.join(asset_path, 'js/zzz_last/activate-foundation.js'), 'client');
 
-// });
+});
